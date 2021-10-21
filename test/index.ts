8fd87c11abf02b5440c6778650a8d7adfb1a2ab8
Renzo Barrios <renzobarrios@aol.com>
2021-10-21 01:39:38 -0300

    Added createTransfer(), getTransfers() + testing

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,49 @@
 import { expect } from "chai";
+import { Contract, ContractFactory } from "ethers";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+//describe("Greeter", function () {
+//it("Should return the new greeting once it's changed", async function () {
+//const Greeter = await ethers.getContractFactory("Greeter");
+//const greeter = await Greeter.deploy("Hello, world!");
+//await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+//expect(await greeter.greet()).to.equal("Hello, world!");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+//const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+//// wait until the transaction is mined
+//await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+//expect(await greeter.greet()).to.equal("Hola, mundo!");
+//});
+//});
+
+describe("Wallet", function () {
+  it("Should create 3 transfers and return the list of transfers", async function () {
+    const [_, rnd1, rnd2, rnd3] = await ethers.getSigners();
+
+    const WalletFactory: ContractFactory = await ethers.getContractFactory(
+      "Wallet"
+    );
+
+    const walletContract: Contract = await WalletFactory.deploy(
+      [rnd1.address, rnd2.address, rnd3.address],
+      1
+    );
+    await walletContract.deployed();
+
+    const transfer1 = await walletContract.createTransfer(10, rnd1.address);
+    transfer1.wait();
+
+    const transfer2 = await walletContract.createTransfer(3, rnd2.address);
+    transfer2.wait();
+
+    const transfer3 = await walletContract.createTransfer(10, rnd3.address);
+    transfer3.wait();
+
+    const transfersTxn = await walletContract.getTransfers();
+
+    expect(transfersTxn.length).to.equal(3);
   });
 });
