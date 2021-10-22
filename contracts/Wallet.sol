4e0f08dbfa55b515adb26d2baee125cfa116afbc
Renzo Barrios <renzobarrios@aol.com>
2021-10-21 21:18:46 -0300

    unit test for contract initialization, createTranfer, getTransfers

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -20,29 +20,19 @@ contract Wallet {
     }
 
     Transfer[] public transfers;
-    uint nextId;
+    mapping(address => mapping(uint => bool)) public approvals;
 
-    constructor(address[] memory _approvers, uint256  _quorum){
+    constructor(address[] memory _approvers, uint256  _quorum) payable{
         approvers = _approvers;
         quorum = _quorum;
-
-        require((approvers.length / 2) >= quorum);
-        console.log("Contract Started");
-    }
-
-    function getApprovers() external view returns (address[] memory){
-        return approvers;
     }
 
 
-    function getTransfers() external view returns (Transfer[] memory){
-        return transfers;
-    }
-
+    // only approvers specified in the constructor can call createTransfer
     function createTransfer(
         uint _amount,
         address payable _to
-        ) external payable returns(Transfer memory) {
+        ) external onlyApprover() payable returns(Transfer memory) {
             
 
         Transfer memory newTransfer = Transfer({
@@ -57,6 +47,48 @@ contract Wallet {
 
 
         return newTransfer;
+    }
+
+
+    function approveTransfer(uint id) external onlyApprover() {
+        require(transfers[id].sent == false,"Transfer already been sent");
+        require(approvals[msg.sender][id] == false, "Cannot approve transfers twice");
+
+        approvals[msg.sender][id] = true;
+        transfers[id].approvals++;
+
+        if( transfers[id].approvals >= quorum ){
+                transfers[id].sent = true;
+                uint amount = transfers[id].amount;
+                address payable to = transfers[id].to;
+                to.transfer(amount);
+        }
+    }
+
+
+    // getters
+    function getApprovers() external view returns (address[] memory){
+        return approvers;
+    }
+
+    function getTransfers() external view returns (Transfer[] memory){
+        return transfers;
+    }
+
+    // receive ether native way
+    receive() external payable {}
+
+    modifier onlyApprover(){
+        bool allowed = false;
+
+        for(uint i = 0; i < approvers.length; i++){
+            if(approvers[i] == msg.sender) {
+                allowed = true;
+            }
+        }
+
+        require(allowed == true, "only approver allowed");
+        _;
     }
 
     
--- test/index.ts
+++ test/index.ts
@@ -1,49 +1,81 @@
 import { expect } from "chai";
-import { Contract, ContractFactory } from "ethers";
+import { Contract, ContractFactory, BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
-//describe("Greeter", function () {
-//it("Should return the new greeting once it's changed", async function () {
-//const Greeter = await ethers.getContractFactory("Greeter");
-//const greeter = await Greeter.deploy("Hello, world!");
-//await greeter.deployed();
-
-//expect(await greeter.greet()).to.equal("Hello, world!");
-
-//const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-//// wait until the transaction is mined
-//await setGreetingTx.wait();
-
-//expect(await greeter.greet()).to.equal("Hola, mundo!");
-//});
-//});
-
 describe("Wallet", function () {
-  it("Should create 3 transfers and return the list of transfers", async function () {
-    const [_, rnd1, rnd2, rnd3] = await ethers.getSigners();
+  let walletContract: Contract;
+  let users: any[];
+
+  this.beforeEach(async () => {
+    users = await ethers.getSigners();
+    const [_, rnd1, rnd2, rnd3, unauthorized] = users;
 
     const WalletFactory: ContractFactory = await ethers.getContractFactory(
       "Wallet"
     );
 
-    const walletContract: Contract = await WalletFactory.deploy(
+    walletContract = await WalletFactory.deploy(
       [rnd1.address, rnd2.address, rnd3.address],
-      1
+      2,
+      {
+        value: ethers.utils.parseEther("100"),
+      }
     );
     await walletContract.deployed();
+  });
 
-    const transfer1 = await walletContract.createTransfer(10, rnd1.address);
+  it("Should have correct approvers and quorum", async () => {
+    const [_, rnd1, rnd2, rnd3] = users;
+    const approvers = await walletContract.getApprovers();
+    const quorum = await walletContract.quorum();
+
+    expect(approvers.length).to.equal(3);
+    expect(approvers[0] === rnd1.address).to.equal(true);
+    expect(approvers[1] === rnd2.address).to.equal(true);
+    expect(approvers[2] === rnd3.address).to.equal(true);
+    expect(quorum.toNumber()).to.equal(2);
+  });
+
+  it("Should create 3 transfers and return the list of transfers", async function () {
+    const [_, rnd1, rnd2, rnd3] = users;
+
+    const transfer1 = await walletContract
+      .connect(rnd1)
+      .createTransfer(10, rnd2.address);
     transfer1.wait();
 
-    const transfer2 = await walletContract.createTransfer(3, rnd2.address);
+    const transfer2 = await walletContract
+      .connect(rnd2)
+      .createTransfer(3, rnd1.address);
     transfer2.wait();
 
-    const transfer3 = await walletContract.createTransfer(10, rnd3.address);
+    const transfer3 = await walletContract
+      .connect(rnd3)
+      .createTransfer(10, rnd2.address);
     transfer3.wait();
 
     const transfersTxn = await walletContract.getTransfers();
 
     expect(transfersTxn.length).to.equal(3);
+    // Test genrated id
+    expect(transfersTxn[0].id.toNumber() === 0).to.equal(true);
+    expect(transfersTxn[1].id.toNumber() === 1).to.equal(true);
+    expect(transfersTxn[2].id.toNumber() === 2).to.equal(true);
+
+    // Test correct amount of each transfer
+    expect(transfersTxn[0].amount.toString() === "10").to.equal(true);
+    // Transfer when is created should be set to sent=false
+    expect(transfersTxn[0].sent === false).to.equal(true);
+    // Should set the correct address to
+    //
+    expect(transfersTxn[0].to === rnd2.address).to.equal(true);
+  });
+
+  it("Should FAIL when an unauthorized sender try to create a transfer", async () => {
+    const [_, rnd1, rnd2, rnd3, unauthorized] = users;
+
+    await expect(
+      walletContract.connect(unauthorized).createTransfer(10, rnd2.address)
+    ).to.be.revertedWith("only approver allowed");
   });
 });
